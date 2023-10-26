fa5e5dc312c088c11e5a73b41558ce2900f3de3f
TPL777 <143000724+TPL777@users.noreply.github.com>
2023-10-26 20:39:55 -0300

    Update exerciciosDeFixacao20231026.sql
    Trigger para atualização do nome de um cliente na tabela Clientes:

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -18,3 +18,16 @@ BEGIN
     VALUES (CONCAT('Tentativa de exclusão de cliente em ', NOW()));
 //
 DELIMITER ;
+
+DELIMITER //
+CRETE TRIGGER atualiza_nome_cliente
+AFTER UPDATe ON Clientes
+FOR EACh ROW
+BEgIN
+    IF NEW.nome != OLD.nome THEN
+        INSERT INTO Auditoria (mensagem)
+        VALUES (CONCAT('Nome do cliente atualizado de "', OLD.nome, '" para "', NEW.nome, '" em ', NOW()));
+    END iF;
+END;
+//
+DELIMITER ;

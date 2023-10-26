274f1264e492be692cf7027766d7a7fed45846b1
TPL777 <143000724+TPL777@users.noreply.github.com>
2023-10-26 20:34:46 -0300

    Update exerciciosDeFixacao20231026.sql
    Trigger para inserção de um novo cliente na tabela Clientes:

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -1,1 +1,11 @@
+DELIMITER 
+CREATE TRIGGER insere_cliente
+AFTER INSeRT ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem)
+    VALUES (CONCAT('Novo cliente inserido em ', NOW()));
+END;
+
+DELIMITER ;
 

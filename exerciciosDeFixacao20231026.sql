090cd988eb0e9d270a65cce9255ecb86e1fcc5d3
TPL777 <143000724+TPL777@users.noreply.github.com>
2023-10-26 20:37:46 -0300

    Update exerciciosDeFixacao20231026.sql
    Trigger para tentativa de exclusão de um cliente na tabela Clientes:

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -1,11 +1,20 @@
-DELIMITER 
+DELIMITER //
 CREATE TRIGGER insere_cliente
 AFTER INSeRT ON Clientes
 FOR EACH ROW
 BEGIN
     INSERT INTO Auditoria (mensagem)
     VALUES (CONCAT('Novo cliente inserido em ', NOW()));
-END;
+END;//
 
 DELIMITER ;
 
+DELIMITER //
+CREATE TRIGGER tentativa_exclusao_cliente
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem)
+    VALUES (CONCAT('Tentativa de exclusão de cliente em ', NOW()));
+//
+DELIMITER ;
